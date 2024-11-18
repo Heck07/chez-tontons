13886f41beb98cc621e31fca2c6e0539d60f98f9
Heck07 <roms904@yahoo.fr>
2024-11-18 23:56:07 +0100

    Add dist subtree

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import '@/assets/styles/global.css'; // Importation du fichier CSS global
-
-
-createApp(App).use(router).mount('#app');
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://github.com/Heck07/chez-tontons' // Remplacez cela par le nom de votre dépôt
-    : '/'
-};
